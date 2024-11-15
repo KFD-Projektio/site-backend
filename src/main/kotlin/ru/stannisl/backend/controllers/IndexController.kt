b5aa778796234b9786fe419f20c1154bcb382b3d
Stannis Latanya <stannisl@student.21-school.ru>
2024-11-16 02:50:40 +0300

    Initial commit

---
+++ src/main/kotlin/ru/stannisl/backend/controllers/IndexController.kt
@@ -0,0 +1,15 @@
+package ru.stannisl.backend.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class IndexController {
+    @ResponseBody
+    @GetMapping("/index")
+    fun index(@RequestParam name: String?): String {
+        return "Hello, $name!"
+    }
+}
