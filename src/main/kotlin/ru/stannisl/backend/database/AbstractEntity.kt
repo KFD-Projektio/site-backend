016a62053503233925e1ef5bc04398c7d410a170
Stannis Latanya <stannisl@student.21-school.ru>
2024-12-21 01:19:17 +0300

    Added Users Table Entity and set some settings

--- src/main/kotlin/ru/stannisl/backend/database/AbstractEntity.kt
+++ src/main/kotlin/ru/stannisl/backend/database/AbstractEntity.kt
@@ -1,4 +1,16 @@
 package ru.stannisl.backend.database
 
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import java.time.LocalDateTime
+
+@MappedSuperclass
 abstract class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
+
+    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    val createdAt : LocalDateTime = LocalDateTime.now()
 }
--- src/main/kotlin/ru/stannisl/backend/database/User.kt
+++ src/main/kotlin/ru/stannisl/backend/database/User.kt
@@ -1,4 +1,18 @@
 package ru.stannisl.backend.database
 
-class User {
-}
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "`user`")
+class User(
+    @Column(name = "password_hash", nullable = false)
+    var passwordHash: String,
+
+    @Column(name = "email", nullable = false)
+    var email: String,
+
+    @Column(name = "login", nullable = false)
+    var login: String,
+) : AbstractEntity() {}
